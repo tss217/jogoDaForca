4713a90f4aaa9e7f35046a58049ed2f696df1ece
Ts_217 <67065618+tss217@users.noreply.github.com>
2023-10-26 13:02:34 -0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -2,18 +2,21 @@
 #include<string>
 #include<map>
 #include<vector>
+#include<fstream>
+#include<ctime>
+#include<cstdlib>
 
 
 using namespace std;
 
-const string PALAVRA_SECRETA = "MELANCIA";
+string palavraSecreta = "MELANCIA";
 
 map<char,bool>chutou;
 vector <char> chutesErrados;
 
 bool letraExistes(char chute){
 
-    for (char letra : PALAVRA_SECRETA)
+    for (char letra : palavraSecreta)
     {
         if(chute == letra){
             return true;
@@ -21,45 +24,102 @@ bool letraExistes(char chute){
     }return false;   
 }
 
+bool naoAcertou(){
+    for(char letra : palavraSecreta){
+        if(!chutou[letra]){
+            return true;
+        }
+    }return false;
+}
+
+bool naoEnforcou(){
+    return chutesErrados.size() < 5;
+}
+
+void imprimeErros(){
+    cout << "chutes errados:";
+    for(char letra : chutesErrados){
+        cout << letra << " ";
+    }
+
+    cout << endl;    
+}
+
+void imprimePalavra(){
+    for(char letra : palavraSecreta){
+                if(chutou[letra]){
+                    cout<< letra << " ";
+                }else{
+                    cout <<"_ ";
+                }
+            }
+            cout<< endl;
+}
+
+void chuta(){
+    cout << "seu chute: ";
+    char chute;
+    cin >> chute;
+
+    chutou[chute]= true;
+
+    if (letraExistes(chute))
+    {
+        cout  << "você acertou seu chute esta na palavra ." << endl;
+
+    }else{
+        cout << " você errou! seu chute não esta na palavra." << endl;
+        chutesErrados.push_back(chute);
+    }
+    cout << endl;    
+}
+
+vector <string> leArquivo(){
+    ifstream arquivo;
+    arquivo.open("palavras.txt");
+
+    int qdtPalavras;
+    arquivo >> qdtPalavras;
+
+    vector <string> palavrasDoArquivo;
+
+    for(int i = 0; i< qdtPalavras;i++){
+        string palavraLida;
+        arquivo >> palavraLida;
+        palavrasDoArquivo.push_back(palavraLida);
+    }return palavrasDoArquivo;
+}
+
+void sorteiaPalavra(){
+    vector <string> palavras = leArquivo();
+
+    srand(time(NULL));
+    int iSorteado = rand() % palavras.size();
+
+    palavraSecreta = palavras[iSorteado];
+}
+
 int main(){
 
+    leArquivo();
+    sorteiaPalavra();
 
-    bool naoAcertou = true;
-    bool naoEnforcou = true;
+    while (naoAcertou() && naoEnforcou()){
 
-    while (naoAcertou && naoEnforcou){
+        imprimeErros();
+        imprimePalavra();
+        
+        chuta();
 
-        cout << "chutes errados:";
-        for(char letra : chutesErrados){
-            cout << letra << " ";
-        }
+    }
 
-        cout << endl;
+    cout << "fim de jogo" << endl;
+    cout << "palavra secreta era:" << palavraSecreta << endl;
 
-        for(char letra : PALAVRA_SECRETA){
-            if(chutou[letra]){
-                cout<< letra << " ";
-            }else{
-                cout <<"_ ";
-            }
-        }
-        cout<< endl;
-
-        cout << "seu chute: ";
-        char chute;
-        cin >> chute;
-
-        chutou[chute]= true;
-
-        if (letraExistes(chute))
-        {
-            cout  << "você acertou seu chute esta na palavra ." << endl;
-
-        }else{
-            cout << " você errou! seu chute não esta na palavra." << endl;
-            chutesErrados.push_back(chute);
-        }
-        cout << endl;
+    if(naoAcertou()){
+        cout << "você perdeu" << endl;
+    }else{
+        cout << "boa" << endl;
     }
     
 }
